2ca3b9d5bd8e57ed45fcbe0d35b4f93012fabeb9
ilhamsetiad1 <ilhamsetiadi747@gmail.com>
2025-06-11 07:12:17 +0700

    nn

--- perpustakaan_mini.java
+++ perpustakaan_mini.java
@@ -13,7 +13,7 @@ public class perpustakaan_mini {
         boolean selesai = false;
 
         while (!selesai) {
-            System.out.println("=== PERUSTAAAN ===");
+            System.out.println("===  ===");
             System.out.println("1. Admin");
             System.out.println("2. Pelanggan");
             System.out.print("Pilih sebagai (1/2): ");
